f8751783e293e378ea70103c8d099700fd700556
NguyenVanNhan-09 <nhan09122004@gmail.com>
2024-07-02 02:45:10 +0700

    Admin (route & dl-<dashboard + listProducts>)

--- src/layout/Admin.jsx
+++ src/layout/Admin.jsx
@@ -1,7 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "../page/dashboard";
+import ListProducts from "../page/ListProduct";
+
 export default function Admin() {
    return (
       <>
-         <h1>Đây là admin</h1>
+         <div class="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
+            <div class="h-32 rounded-lg bg-gray-200">
+               <Dashboard />
+            </div>
+            <div class="h-32 rounded-lg bg-gray-200 lg:col-span-3">
+               <Routes>
+                  <Route path="/" element={<Navigate to="products-list" />} />
+                  <Route path="products-list" element={<ListProducts />} />
+               </Routes>
+            </div>
+         </div>
       </>
    );
 }
