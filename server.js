b7cdad683ed3477509f33f06fe9319c5105afebf
NguyenVanNhan-09 <nhan09122004@gmail.com>
2024-07-17 04:32:53 +0700

    test deploy render

---
+++ server.js
@@ -0,0 +1,18 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+// /!\ Bind the router db to the app
+server.db = router.db;
+
+// You must apply the auth middleware before the router
+server.use(middlewares);
+server.use(auth);
+server.use(router);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+   console.log(`JSON Server is running on port ${port}`);
+});
